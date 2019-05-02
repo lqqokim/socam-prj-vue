07d165612fbcb5f50de15f2001ca4c9d6de3d66d
kiminsu <kis420404@gmail.com>
2019-05-03 05:52:35 +0900

    feat(main): main page layout

---
+++ src/views/Main.vue
@@ -0,0 +1,158 @@
+<template>
+  <v-app>
+    <v-toolbar class="toolbar" color="gray" dark extended>
+      <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+      <v-avatar color="grey lighten-4">
+        <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+      </v-avatar>
+      <v-toolbar-title class="white--text font-weight-medium display-2">Software Campus</v-toolbar-title>
+
+      <template v-slot:extension>
+        <!-- <v-toolbar-title class="white--text">Software Campus</v-toolbar-title> -->
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>About</v-btn>
+          <v-btn flat>Community</v-btn>
+          <v-btn flat>Event</v-btn>
+          <v-btn flat>Source Store</v-btn>
+          <v-btn flat>Notice</v-btn>
+          <v-btn flat>My Page</v-btn>
+        </v-toolbar-items>
+      </template>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-parallax
+      class="parallex"
+      height="400"
+      dark
+      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+    >
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 font-weight-thin mb-3">WELCOME SOCAM!</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-layout>
+    </v-parallax>
+
+    <!-- <v-card>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex v-for="card in cards" :key="card.title" v-bind="{ [`xs${card.flex}`]: true }">
+            <v-card>
+              <v-img :src="card.src" height="200px">
+                <v-container fill-height fluid pa-2>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>bookmark</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>share</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>-->
+
+    <v-footer class="footer" dark height="auto">
+      <v-card flat tile class="indigo lighten-1 white--text text-xs-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text
+          class="white--text pt-0"
+        >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          &copy;2018 —
+          <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 12
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 6
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 6
+      }
+    ],
+    icons: [
+      "fab fa-facebook",
+      "fab fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+  })
+};
+</script>
+
+<style scoped>
+.toolbar {
+  /* background-color: gray !important; */
+  opacity: 0.5;
+  z-index: 1;
+}
+
+.parallex {
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+}
+</style>

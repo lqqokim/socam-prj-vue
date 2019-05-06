59c83fc4b935600fb0d0816ffcae610aae345f1f
kiminsu <kis420404@gmail.com>
2019-05-06 19:39:25 +0900

    feat(toolbar): create toolbar component

---
+++ src/components/toolbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-toolbar class="toolbar" color="gray" dark extended>
+    <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+    <v-avatar color="grey lighten-4">
+      <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+    </v-avatar>
+    <v-toolbar-title class="white--text font-weight-medium display-2">Software Campus</v-toolbar-title>
+
+    <template v-slot:extension>
+      <!-- <v-toolbar-title class="white--text">Software Campus</v-toolbar-title> -->
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>About</v-btn>
+        <v-btn flat>Community</v-btn>
+        <v-btn flat>Event</v-btn>
+        <v-btn flat>Source Store</v-btn>
+        <v-btn flat>Notice</v-btn>
+        <v-btn flat>My Page</v-btn>
+      </v-toolbar-items>
+    </template>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'toolbar'
+}
+
+</script>
+
+<style>
+
+</style>
+
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,43 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar class="toolbar" color="gray" dark extended>
-      <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
-      <v-avatar color="grey lighten-4">
-        <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
-      </v-avatar>
-      <v-toolbar-title class="white--text font-weight-medium display-2">Software Campus</v-toolbar-title>
-
-      <template v-slot:extension>
-        <!-- <v-toolbar-title class="white--text">Software Campus</v-toolbar-title> -->
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn flat>About</v-btn>
-          <v-btn flat>Community</v-btn>
-          <v-btn flat>Event</v-btn>
-          <v-btn flat>Source Store</v-btn>
-          <v-btn flat>Notice</v-btn>
-          <v-btn flat>My Page</v-btn>
-        </v-toolbar-items>
-      </template>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>refresh</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <Toolbar/>
 
     <v-parallax
       class="parallex"
@@ -108,6 +71,8 @@
 </template>
 
 <script>
+import Toolbar from "./../components/toolbar.vue";
+
 export default {
   data: () => ({
     cards: [
@@ -134,7 +99,10 @@ export default {
       "fab fa-linkedin",
       "fab fa-instagram"
     ]
-  })
+  }),
+  components: {
+    Toolbar
+  }
 };
 </script>
 
@@ -152,7 +120,7 @@ export default {
 }
 
 .footer {
-    position: absolute;
-    bottom: 0;
+  position: absolute;
+  bottom: 0;
 }
 </style>

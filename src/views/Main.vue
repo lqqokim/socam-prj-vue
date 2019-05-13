8b22a123e95b44adb79dc8439b9e9acbdd0148d9
kiminsu <kis420404@gmail.com>
2019-05-13 23:54:45 +0900

    feat: configuring the main page layout

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,72 +1,185 @@
 <template>
-  <v-app>
+  <v-app light>
+    <!-- <v-progress-linear class="progress" :indeterminate="true" height="5"></v-progress-linear> -->
+
+    <v-progress-linear :indeterminate="true" class="ma-0" height="5"></v-progress-linear>
+
+    <!-- <v-toolbar class="white">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>-->
     <Toolbar/>
 
-    <v-parallax
-      class="parallex"
-      height="400"
-      dark
-      src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-    >
-      <v-layout align-center column justify-center>
-        <h1 class="display-2 font-weight-thin mb-3">WELCOME SOCAM!</h1>
-        <h4 class="subheading">Build your application today!</h4>
-      </v-layout>
-    </v-parallax>
+    <v-content>
+      <section>
+        <v-parallax :src="getImage('hero.jpeg')" height="400">
+          <v-layout column align-center justify-center class="white--text">
+            <img :src="getImage('vuetify.png')" alt="Vuetify.js" height="200">
+            <h1 class="white--text mb-2 display-1 text-xs-center">안녕하세요 소프트웨어캠퍼스 입니다.</h1>
+            <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+            <!-- <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">Get Started</v-btn> -->
+          </v-layout>
+        </v-parallax>`
+      </section>
 
-    <!-- <v-card>
-      <v-container fluid grid-list-md>
-        <v-layout row wrap>
-          <v-flex v-for="card in cards" :key="card.title" v-bind="{ [`xs${card.flex}`]: true }">
-            <v-card>
-              <v-img :src="card.src" height="200px">
-                <v-container fill-height fluid pa-2>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline white--text" v-text="card.title"></span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
+      <section>
+        <v-layout column wrap class="my-5" align-center>
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">The best way to start developing</h2>
+              <span class="subheading">Cras facilisis mi vitae nunc</span>
+            </div>
+          </v-flex>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>favorite</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>bookmark</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>share</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card>
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Tensoflow 2.0</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                      potenti.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">클라우드 네이티브 자바</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                      potenti.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Spring Boot 2.0</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                      potenti.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
           </v-flex>
         </v-layout>
-      </v-container>
-    </v-card>-->
+      </section>
 
-    <v-footer class="footer" dark height="auto">
-      <v-card flat tile class="indigo lighten-1 white--text text-xs-center">
-        <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
-        </v-card-text>
+      <section>
+        <v-parallax :src="getImage('section.jpg')" height="300">
+          <v-layout column align-center justify-center>
+            <div
+              class="headline white--text mb-3 text-xs-center"
+            >소프트웨어 캠퍼스는 여러분을 응원합니다.</div>
+            <em>Kick-start your application today</em>
+            <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">Get Started</v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
 
-        <v-card-text
-          class="white--text pt-0"
-        >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Company info</div>
+                </v-card-title>
+                <v-card-text>
+                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                  potenti.
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Contact us</div>
+                </v-card-title>
+                <v-card-text>Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.</v-card-text>
+                <v-list class="transparent">
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">phone</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>777-867-5309</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">place</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>Chicago, US</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon class="blue--text text--lighten-2">email</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
 
-        <v-divider></v-divider>
+      <v-footer class="footer" dark height="auto">
+        <v-card flat tile class="indigo lighten-1 white--text text-xs-center">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+          </v-card-text>
 
-        <v-card-text class="white--text">
-          &copy;2018 —
-          <strong>Vuetify</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+          <v-card-text
+            class="white--text pt-0"
+          >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">
+            &copy;2018 —
+            <strong>Vuetify</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+      <!-- <v-footer class="blue darken-2">
+          <v-layout row wrap align-center>
+            <v-flex xs12>
+              <div class="white--text ml-3">
+                Made with
+                <v-icon class="red--text">favorite</v-icon>
+                by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+                and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+              </div>
+            </v-flex>
+          </v-layout>
+      </v-footer>-->
+    </v-content>
   </v-app>
 </template>
 
@@ -75,6 +188,7 @@ import Toolbar from "./../components/toolbar.vue";
 
 export default {
   data: () => ({
+    title: "Software Campus",
     cards: [
       {
         title: "Pre-fab homes",
@@ -100,6 +214,11 @@ export default {
       "fab fa-instagram"
     ]
   }),
+  methods: {
+    getImage(img) {
+      return require(`../assets/images/${img}`);
+    }
+  },
   components: {
     Toolbar
   }
@@ -118,9 +237,13 @@ export default {
   z-index: 0;
   width: 100%;
 }
-
+  
 .footer {
   position: absolute;
   bottom: 0;
+}
+  
+.progress {
+  margin: 0;
 }
 </style>
